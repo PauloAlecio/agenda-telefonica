86f2f673c4839230efaaf0db73027fe8c7695f54
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2023-12-18 17:38:02 -0300

    Add definition of class model

---
+++ src/main/java/com/agenda/telefonica/model/Cantato.java
@@ -0,0 +1,36 @@
+package com.agenda.telefonica.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "contato")
+public class Cantato implements Serializable
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long Id;
+
+
+    @Column(name="nome")
+    private String nome;
+
+    @Column(name="e-mail")
+    private String email;
+
+
+    @Column(name="telefone")
+    private String telefone;
+
+
+}
