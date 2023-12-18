e4a6d071adad3661bad2980d144d2c18b8615a1f
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2023-12-18 17:11:09 -0300

    Add configuration

---
+++ src/test/java/com/agenda/telefonica/AgendaTelefonicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agenda.telefonica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaTelefonicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
