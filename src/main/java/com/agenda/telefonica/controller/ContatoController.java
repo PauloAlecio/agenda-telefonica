687fe3f58e6c8bc570698c4074caabfbb722fa77
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2023-12-18 18:58:52 -0300

    Add contatoController

---
+++ src/main/java/com/agenda/telefonica/controller/ContatoController.java
@@ -0,0 +1,36 @@
+package com.agenda.telefonica.controller;
+
+import com.agenda.telefonica.model.Contato;
+import com.agenda.telefonica.service.ContatoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ContatoController
+{
+
+    private ContatoService service;
+
+    @Autowired
+    public ContatoController ( ContatoService service ) {
+        this.service = service;
+    }
+
+    @GetMapping("/contato")
+    public String getForm( Model model ){
+        model.addAttribute("contato",new Contato());
+        return "contato";
+    }
+
+
+     @PostMapping("/contato")
+    public String submmit(@ModelAttribute Contato contato, Model model ){
+        model.addAttribute("contato",contato);
+        service.add(contato);
+        return "result";
+     }
+}
