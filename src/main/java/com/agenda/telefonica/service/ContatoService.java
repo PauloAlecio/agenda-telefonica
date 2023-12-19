8d392865fe9bc1f83e7a3d814324d930766ee2bc
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2023-12-18 21:54:49 -0300

    feat: update INTERFACE SERVICE
    Adicioana método listar contatos

--- src/main/java/com/agenda/telefonica/service/ContatoService.java
+++ src/main/java/com/agenda/telefonica/service/ContatoService.java
@@ -2,7 +2,10 @@ package com.agenda.telefonica.service;
 
 import com.agenda.telefonica.model.Contato;
 
+import java.util.List;
+
 public interface ContatoService
 {
     void add ( Contato model );
+    List<Contato> getContatos();
 }
